b33722a38922669e3923ff56886da177a4cbce7c
mxr <eetu.kivelae@gmail.com>
2022-02-21 04:04:16 +0200

    Work on implementing choice system visuals

--- src/Pages.elm
+++ src/Pages.elm
@@ -1,23 +1,24 @@
 module Pages exposing (katakana, hiragana, kanji, numbers, combined)
 
-import Html exposing (h1, div, text)
+import Html exposing (h1, div, text, button)
 import Html.Attributes exposing (class)
 
 import Shared exposing (Msg(..), Model)
 
 
 katakana : Model -> Html.Html Msg
-katakana _ =
-  div [ class "flex p-4" ]
+katakana model =
+  div [ class "flex flex-col items-center p-4" ]
     [ 
-      h1 [] [ text "katakana"]
+      glyph_showcase model "カ",
+      choices_container model
     ]
 
 
 
 hiragana : Model -> Html.Html Msg
 hiragana _ =
-  div [ class "flex p-4" ]
+  div [ class "flex flex-col p-4" ]
     [ 
       h1 [] [ text "hiragana"]
     ]
@@ -25,7 +26,7 @@ hiragana _ =
 
 kanji : Model -> Html.Html Msg
 kanji _ =
-  div [ class "flex p-4" ]
+  div [ class "flex flex-col p-4" ]
     [ 
       h1 [] [ text "kanji"]
     ]
@@ -33,17 +34,38 @@ kanji _ =
 
 numbers : Model -> Html.Html Msg
 numbers _ =
-  div [ class "flex p-4" ]
+  div [ class "flex flex-col p-4" ]
     [ 
       h1 [] [ text "numbers"]
     ]
 
 combined : Model -> Html.Html Msg
 combined model =
-  div [ class "flex p-4" ]
+  div [ class "flex flex-col p-4" ]
     [ 
       hiragana model,
       katakana model,
       kanji model,
       numbers model
     ]
+
+
+glyph_showcase : Model -> String -> Html.Html Msg
+glyph_showcase _ glyph =
+  div [ class "text-center bg-platinum text-auburn w-28 h-28 m-4 rounded-sm drop-shadow-md" ]
+    [ h1 [ class "select-none font-Kosugi h-full w-full flex justify-center items-center text-5xl" ] [ text glyph ] ]
+
+choices_container : Model -> Html.Html Msg
+choices_container model =
+  div [ class "flex flex-row" ]
+    [
+      choice_button model "ga",
+      choice_button model "zu",
+      choice_button model "shi",
+      choice_button model "ka"
+    ]
+
+choice_button : Model -> String -> Html.Html Msg
+choice_button _ choice =
+  div [ class "text-center bg-platinum text-auburn w-14 h-14 m-4 rounded-sm drop-shadow-md" ]
+    [ button [ class "select-none font-PT-Sans h-full w-full flex justify-center items-center text-2xl" ] [ text choice ] ]
