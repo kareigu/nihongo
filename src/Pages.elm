364c07ccf8e14d1d50fd5295e0ba985e6e4d438b
mxr <eetu.kivelae@gmail.com>
2022-02-21 20:38:00 +0200

    Made random draws display

--- src/Pages.elm
+++ src/Pages.elm
@@ -12,8 +12,7 @@ katakana : Model -> Html.Html Msg
 katakana model =
   div [ class "flex flex-col items-center p-4" ]
     [ 
-      glyph_showcase model "カ",
-      choices_container model
+      picking_view model
     ]
 
 
@@ -51,46 +50,43 @@ combined model =
       numbers model
     ]
 
-{- random_glyphs : Array Glyph -> Maybe ChoiceData
-random_glyphs glyphs =
-  let (c, ch) = (get_random_glyphs glyphs)
-  in
-    {}
 
-get_random_glyph : Array Glyph -> Maybe Glyph
-get_random_glyph glyphs =
-  let generator = (Random.int 0 (Array.length glyphs - 1))
-  in
-    (Array.get Random.map glyphs)
+picking_view : Model -> Html.Html Msg
+picking_view model =
+  div []
+    [
+      case model.choice_data.current of
+        Nothing ->
+          div [ class "flex flex-col items-center" ]
+            [
+              text "Nothing rolled"
+            ]
 
-get_random_glyphs : Array Glyph -> ((Maybe Glyph), ((Maybe Glyph), (Maybe Glyph), (Maybe Glyph)))
-get_random_glyphs glyphs =
-  (
-    get_random_glyph glyphs,
-    (
-      get_random_glyph glyphs,
-      get_random_glyph glyphs,
-      get_random_glyph glyphs
-    )
-  ) -}
-
+        Just x ->
+          div [ class "flex flex-col items-center" ]
+            [
+              glyph_showcase model (Tuple.first x.correct),
+              choices_container model x.choices
+            ]
+    ]
 
 glyph_showcase : Model -> String -> Html.Html Msg
 glyph_showcase _ glyph =
   div [ class "text-center bg-platinum text-auburn w-28 h-28 m-4 rounded-sm drop-shadow-md" ]
-    [ h1 [ class "select-none font-Kosugi h-full w-full flex justify-center items-center text-5xl" ] [ text glyph ] ]
-
-choices_container : Model -> Html.Html Msg
-choices_container model =
-  div [ class "flex flex-row" ]
-    [
-      choice_button model "ga",
-      choice_button model "zu",
-      choice_button model "shi",
-      choice_button model "ka"
+    [ h1 [ class "select-none font-Kosugi h-full w-full flex justify-center items-center text-5xl" ] 
+      [ text glyph ] 
     ]
 
-choice_button : Model -> String -> Html.Html Msg
-choice_button _ choice =
+choices_container : Model -> List Glyph -> Html.Html Msg
+choices_container model choices =
+  div [ class "flex flex-row" ]
+    (List.map make_choice_button choices)
+
+make_choice_button : Glyph -> Html.Html Msg
+make_choice_button glyph =
+  choice_button (Tuple.second glyph)
+
+choice_button : String -> Html.Html Msg
+choice_button choice =
   div [ class "text-center bg-platinum text-auburn w-14 h-14 m-4 rounded-sm drop-shadow-md" ]
     [ button [ class "select-none font-PT-Sans h-full w-full flex justify-center items-center text-2xl" ] [ text choice ] ]
